6c1f1434a8b751210f0e0b193bfa2d14c6803b9f
Yashaswi <yashasvi1707@gmail.com>
2024-09-14 09:00:11 +0530

    clean some code

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { PORT } from "./constant.js";
-import roomRouter from "./Router/room.router.js";
+import { getPriceForNext30Days } from "./controllers/price.controller.js";
+import { getOccupancyForNext5Months } from "./controllers/occupancy.controller.js";
 
 const app = express();
-app.use("/airbnb", roomRouter);
+
+app.get("/airbnb/rate/:room_id", getPriceForNext30Days);
+app.get("/airbnb/occupancy/:room_id", getOccupancyForNext5Months)
 
 app.listen(PORT, () => {
     console.log("Running on port: ", PORT)
